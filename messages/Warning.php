775e5a2883a0de1c31f23fdbb7a14753c10062d1
HighElo <admin@highelo.pl>
2020-01-14 01:13:28 +0100

    Dodanie ostrzeżenia $valid

--- messages/Warning.php
+++ messages/Warning.php
@@ -3,6 +3,7 @@
 
 	final class Warning {
 		public static $nochanges = 'Dane się nie dodały/zmieniły/usunęły';
+        public static $valid = 'Formularz nie przeszedł walidacji';
         public static $delete = 'Rekord nie został usunięty z bazy danych';
         public static $add = 'Rekord nie został dodany do bazy danych';
         public static $update = 'Rekord nie został zaktualizowany w bazie danych';
