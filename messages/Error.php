3bcc0bd32473511c20cc4fb2416d48557937a3df
HighElo <admin@highelo.pl>
2020-01-14 01:13:04 +0100

    Zmiana treści błędu $login

--- messages/Error.php
+++ messages/Error.php
@@ -7,7 +7,7 @@ final class Error {
     public static $templateNotFound = 'Błąd tworzenia widoku';
     public static $query = 'Błąd zapytania do bazy danych';
     public static $empty = 'Któraś z wartości jest pusta';
-    public static $login = 'Login lub hasło jest niepoprawne!';
+    public static $login = 'Nieprawidłowy login lub hasło!';
     public static $delete = 'Rekord nie został usunięty z bazy danych';
     public static $add = 'Rekord nie został dodany do bazy danych';
     public static $update = 'Rekord nie został zaktualizowany w bazie danych';
