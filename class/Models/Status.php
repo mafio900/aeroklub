5816f9252ad050c594ab79de32f704f0cb87c270
HighElo <admin@highelo.pl>
2020-01-14 18:37:19 +0100

    Dodanie walidacji do Status

--- class/Models/Status.php
+++ class/Models/Status.php
@@ -19,19 +19,25 @@ class Status extends PDODatabase
      * @param  string $name
      * @return array
      */
-    public function insert($name)
+    public function insert($statusnazwa)
     {
         $id = -1;
         $this->testConnection();
         $this->testTable($this->table);
-        if(!isset($name))
+        if( !isset($statusnazwa) ||
+            $statusnazwa = '' ||
+            !preg_match('/^[^\s][a-zA-ZąĄęĘóśŚÓłŁżŻźŹćĆńŃ]*$/', $statusnazwa) ||
+            strlen($statusnazwa) > 50
+        ){
+            \Tools\FlashMessage::addMessage(0, 'valid');
             return 0;
+        }
         try	{
             $query = 'INSERT INTO `'.$this->table.'` (`StatusNazwa`)';
-            $query .= ' VALUES (:name)';
+            $query .= ' VALUES (:statusnazwa)';
             $stmt = $this->pdo->prepare($query);
 
-            $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+            $stmt->bindValue(':statusnazwa', $statusnazwa, PDO::PARAM_STR);
             if($stmt->execute()) {
                 $id = $this->pdo->lastInsertId();
             }
@@ -49,23 +55,32 @@ class Status extends PDODatabase
      * @param  string $name
      * @return int
      */
-    public function update($id, $name)
+    public function update($id, $statusnazwa)
     {
         $this->testConnection();
         $this->testTable($this->table);
 
-        if(!isset($id) && !isset($name))
+        if( !isset($id) ||
+            $id == '' ||
+            !is_numeric($id) ||
+            !isset($statusnazwa) ||
+            $statusnazwa = '' ||
+            !preg_match('/^[^\s][a-zA-ZąĄęĘóśŚÓłŁżŻźŹćĆńŃ]*$/', $statusnazwa) ||
+            strlen($statusnazwa) > 50
+        ){
+            \Tools\FlashMessage::addMessage(0, 'valid');
             return 0;
+        }
         try	{
             $query = 'UPDATE `'.$this->table.'`';
-            $query .= ' SET StatusNazwa = :name';
+            $query .= ' SET StatusNazwa = :statusnazwa';
             $query .= ' WHERE id = :id';
             $stmt = $this->pdo->prepare($query);
 
-            $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+            $stmt->bindValue(':statusnazwa', $statusnazwa, PDO::PARAM_STR);
             $stmt->bindValue(':id', $id, PDO::PARAM_INT);
             if($stmt->execute()) {
-                $id = $this->pdo->lastInsertId();
+                $id = $stmt->rowCount();
             }
             $stmt->closeCursor();
         } catch(\PDOException $e) {
