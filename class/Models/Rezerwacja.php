01f0c7f76b27048a03a4b4d58c0af0f5f12a476e
HighElo <admin@highelo.pl>
2020-01-14 19:15:12 +0100

    Update Rezerwacja.php

--- class/Models/Rezerwacja.php
+++ class/Models/Rezerwacja.php
@@ -70,7 +70,7 @@ class Rezerwacja extends PDODatabase
 
         if( !isset($id) ||
             $id == '' ||
-            !is_numeric($id)
+            !is_numeric($id) ||
             !isset($terminRealizacji) ||
             $terminRealizacji == '' ||
             !preg_match('/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9])$/', $terminRealizacji) ||
